d38d37c9a996f8846bf521a25a43bd5faaf8f871
aannnaaka <93240349+aannnaaka@users.noreply.github.com>
2022-06-07 16:00:30 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,78 @@
+var trex, trex_running;
+var ground, invisibleGround, groundImage;
+//crea una variable para guardar el sprite de nuve y la imagen
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex2.png","trex3.png");
+  groundImage = loadImage("ground2.png");
+  //Precarga la imagen de la nube
+ 
+  
+}
+
+function setup() {
+
+  createCanvas(600,200)
+  
+  //crear sprite de trex
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
+  
+  //crear sprite de suelo
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -4;
+  
+  //crear sprite de suelo invisible
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  //generar números aleatorios
+  var rand =  Math.round(random(1,100))
+  console.log(rand)
+
+}
+
+function draw() {
+  //establecer color de fondo
+  background(180);
+  
+  console.log(trex.y)
+
+  //hacer que el trex salte al presionar la barra espaciadora
+  if(keyDown("space")&& trex.y >= 100) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  //evitar que el trex caiga
+  trex.collide(invisibleGround);
+  
+  //aparecer nubes
+  spawnClouds()
+  
+  drawSprites();
+}
+
+//función para aparecer las nubes
+function spawnClouds(){
+ //escribir aquí el código 
+ 
+ //agrega la imagen a la nube
+
+ //modifica su tamaño
+
+ //agrega velocidad
+
+
+}
+
+
+
